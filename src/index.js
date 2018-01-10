aeeb92761d6d5fd1c8d32b4e6ad37ed81f630b2f
unknown <aatosm@hotmail.com>
2018-01-10 13:57:52 +0200

    Added react-router and prop-types

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
+import PropTypes from 'prop-types';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -11,7 +13,13 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/' component={App} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
  document.getElementById('root')
 );
